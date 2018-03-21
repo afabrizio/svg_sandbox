d1d3705ee47b8d671d9259ddc97fed2bdebf4625
Andrew Fabrizio <afabrizio@cytellix.com>
2018-03-20 17:58:31 -0700

    Completed animation of horizontal widget.

--- src/components/widget.component.jsx
+++ src/components/widget.component.jsx
@@ -5,37 +5,95 @@ import alerts from './alerts.json';
 export class HorizontalWidget extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            animation: undefined,
+            selectedAlert: -1
+        }
     }
 
     componentDidMount() {
+        this.startAnimation();
     }
   
     componentWillUnmount() {
+        this.stopAnimation();
     }
 
     render() {
         return (
-            <div name="horizontal-widget">
-                <span className="bumper x">
+            <div name="horizontal-widget" onMouseEnter={() => this.stopAnimation()} onMouseLeave={() => this.startAnimation()}>
+                <button className="bumper x" onClick={() => this.scrollTo(this.state.selectedAlert - 1)} disabled={this.state.selectedAlert <= 0 ? true : false}>
                     <span className="y">
                         <i className="fa fa-chevron-left"></i>
                     </span>
-                </span>
-                <ul>{alerts.map( (alert, i) => (
-                    <li key={i}>
+                </button>
+                <ul className="alerts">{alerts.map( (alert, i) => (
+                    <li id={'alert' + i} key={i} className="alert" onClick={() => this.scrollTo(i)}>
                         <span className="priority" style={{color: this.translatePriority(alert.priority).color}}>{this.translatePriority(alert.priority).label}</span>
                         <span className="time">{(new Date(alert.timestamp)).toLocaleTimeString()}</span>
                         <span className="date">{(new Date(alert.timestamp)).toDateString()}</span>
-                        <div className="alert">{alert.type}</div>
+                        <div className="type">{alert.type}</div>
                     </li>
                 ))}</ul>
-                 <span className="bumper x">
+                <button className="bumper x" onClick={() => this.scrollTo(this.state.selectedAlert + 1)} disabled={(this.state.selectedAlert >= (alerts.length-1)) ? true : false}>
                     <span className="y">
                         <i className="fa fa-chevron-right"></i>
                     </span>
-                </span>
+                </button>
             </div>
         );
+    }
+
+    scrollTo(index) {        
+        let requestedAlert = document.getElementById('alert' + index);
+        
+        if (requestedAlert) {
+            // updates class values:
+            for (let alert of document.getElementsByClassName('alert')) {
+                alert.classList.remove('selected')
+            }
+            requestedAlert.classList.add('selected');
+            // determines scroll positioning:
+            let positioning;
+            switch (true) {
+                case (index < 1):
+                    positioning = 'start';
+                    break;
+                case (index >= (alerts.length-1)):
+                    positioning = 'end';
+                    break;
+                default:
+                    positioning = 'center';
+            }
+            // initiates scroll animation and updates state:         
+            requestedAlert.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+                inline: positioning
+            });
+            this.setState({
+                selectedAlert: index
+            });
+        }
+    }
+
+    startAnimation() {
+        this.state.animation = window.setInterval( () => {
+            let next = this.state.selectedAlert;
+            switch (true) {
+                case (this.state.selectedAlert < (alerts.length - 1)):
+                    next++
+                    break;
+                default:
+                    next = 0;
+            }
+            this.scrollTo(next);
+        }, 5000);
+    }
+
+    stopAnimation() {
+        window.clearInterval(this.state.animation);
     }
 
     translatePriority(priority) {
